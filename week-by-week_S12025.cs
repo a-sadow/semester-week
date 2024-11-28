e2a9f450093b4f149a56bef5a1c4b2df574a2f99
a-sadow <annie.sadowski@sydney.edu.au>
2024-11-28 12:51:54 +1100

    Update week-by-week_S12025.cs
    Change variable portion text colour

--- week-by-week_S12025.cs
+++ week-by-week_S12025.cs
@@ -5,7 +5,9 @@
   <title>Semester Week</title>
 </head>
 <body>
-  <p style="border: solid gray; padding: 10px; text-align: center; font-family: 'Tw Cen MT', sans-serif; font-size: 16pt;" <style="colour:#236fa1;">id="week-display"></p> 
+  <p style="border: 1px solid gray; padding: 10px; text-align: center; font-family: Lato, sans-serif; font-size: 16pt;">
+   Right now, <span id="week-display" style="color: rgb(22, 22, 138);"></span>
+  </p> 
   <script> // [N.B: if you want to change how the box and text looks in terms of styling, do so in the line above]
 
     const semesterStartDate = new Date("2025-02-25T00:00:00+10:00"); // enter the start date of the Semester in AEDT or AEST
